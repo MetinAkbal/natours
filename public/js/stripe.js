8aeb888f1c6c29c0df2fefb0bad9f41397ca81e1
Metin Akbal <>
2020-10-19 20:44:44 +0200

    Initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,24 @@
+/*eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51HWQbXDs3AXkRbkaXYsKkYvRjFAWUc6ZPQrfCEU4PwVGdqztu1fU4mJmUZ91sqHoohPQrtsSpzyh6dhmCQYGCQ9Q00w6lxcmsZ'
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout from + chanrge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (error) {
+    console.log(error);
+    showAlert('error', error);
+  }
+};
