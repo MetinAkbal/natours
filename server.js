71b70053238bdc33f497c654d85451fdb7c902b5
Metin Akbal <>
2020-10-29 19:48:38 +0100

    Added heroku config

--- server.js
+++ server.js
@@ -36,3 +36,10 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('👋  SIGTERM RECEIVD. shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
