4f8265375702f431c9211587b23e909513c6dcf6
Metin Akbal <>
2020-11-05 20:39:37 +0100

    Fixed alert message

--- public/js/index.js
+++ public/js/index.js
@@ -73,4 +73,4 @@ if (bookBtn)
   });
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alertMessage) showAlert('Success', alertMessage, 20);
+if (alertMessage) showAlert('success', alertMessage, 20);
