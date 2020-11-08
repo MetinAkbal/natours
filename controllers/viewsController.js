151b1b72b8dce651e615ff19ad9d84ed11ea0868
Metin Akbal <>
2020-11-08 16:10:55 +0100

    Fixed alert message4

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -5,7 +5,7 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.alerts = (req, res, next) => {
-  const alert = req.query;
+  const { alert } = req.query;
   if (alert === 'booking')
     res.locals.alert =
       "Your booking was successful! Please check your email for a confirmation. if your booking doesn't show up here immediatly, please come back later";
--- public/js/alerts.js
+++ public/js/alerts.js
@@ -8,7 +8,7 @@ export const hideAlert = () => {
 // type is 'success' or 'error'
 export const showAlert = (type, msg, time = 7) => {
   hideAlert();
-  const merkup = `<div class="alert alert--${type}">${msg}</div>`;
-  document.querySelector('body').insertAdjacentHTML('afterbegin', merkup);
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
   window.setTimeout(hideAlert, time * 1000);
 };
